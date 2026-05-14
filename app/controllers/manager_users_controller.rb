d6f33e6354d22207aa1bb9f8d426f5ea24046e04
Marília Gabriele <marigabi916@gmail.com>
2026-05-13 21:13:01 -0300

    Att: Adicionando roles e dinamica de admin para mudança de roles de usuario

--- app/controllers/manager_users_controller.rb
+++ app/controllers/manager_users_controller.rb
@@ -1,7 +1,34 @@
 class ManagerUsersController < ApplicationController
+    before_action :require_admin!
+    before_action :set_user, only: %i[edit update]
+
+    ALLOWED_ROLES = %w[doctor attendent client admin].freeze
 
     def index
-        @users = User.all
-    end 
+      @users = User.all
+    end
 
-end
+    def edit
+      set_user unless defined?(@user) && @user.present?
+    end
+
+    def update
+      selected = Array(params.dig(:user, :role_names)).map(&:to_s)
+      selected &= ALLOWED_ROLES
+      current = @user.roles.pluck(:name)
+      (selected - current).each { |r| @user.add_role(r) }
+      (current - selected).each { |r| @user.remove_role(r) }
+      @user.add_role(:client) if @user.roles.blank?
+      redirect_to manager_users_path, notice: "Roles atualizadas com sucesso."
+    end
+
+    private
+    def set_user
+      @user = User.find(params[:id])
+    end
+    
+    def require_admin!
+      return if current_user&.has_role?(:admin)
+      redirect_to clinics_path, alert: "Acesso negado."
+    end
+  end
